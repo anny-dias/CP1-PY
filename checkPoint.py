853a048ac53634dfe14538e262994e6dbc1b5eff
anny-dias <annydiascmd@gmail.com>
2023-03-25 20:19:02 -0300

    checkPoint1

---
+++ checkPoint.py
@@ -0,0 +1,34 @@
+#Anny Carolina Andrade Dias - RM:98295
+#Luana Cabezaolias - RM:99320
+
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite outro número: "))
+
+soma = numero1 + numero2
+
+
+a = soma // 10 
+resto1 = soma % 10
+b = resto1 // 1
+
+
+
+if a == 7:
+    print(0)
+elif a != 7:
+    print(a)
+
+if b == 7:
+    print(0)
+elif b != 7:
+    print(b)
+
+
+
+
+
+
+
+
+
+
